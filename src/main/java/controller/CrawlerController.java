2d7e48d4c76a283e751ae13e555dd742f686e600
mihdjo <mihailo.djokovic2003@gmail.com>
2026-06-10 18:26:05 +0200

    main scraper logic added

--- src/main/java/controller/CrawlerController.java
+++ src/main/java/controller/CrawlerController.java
@@ -4,7 +4,10 @@ import data.ScrapedDocument;
 import java.util.Set;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
+import workers.ScraperWorker;
 
 /**
  *
@@ -24,7 +27,37 @@ public class CrawlerController {
         URL_QUEUE.add(seedURL);
         VISITED_SET.add(seedURL);
         
-        //TODO: zapocni scraper workere 
-        //TODO: zapocni mongo writer workere
+        System.out.println("Initializing Column Crawler engine....");
+        
+        int numberOfScrapers = 3; 
+        ExecutorService scraperPool = Executors.newFixedThreadPool(numberOfScrapers);
+        
+        for(int i = 1; i <= numberOfScrapers; i++){
+            String workerId = "SCRAPER THREAD " + i;
+            scraperPool.execute(new ScraperWorker(workerId));
+        }
+        
+        System.out.println(numberOfScrapers + " scraper threads started successfully! \n");
+        
+        while(true){
+            try{
+                ScrapedDocument doc = RESULTS_QUEUE.take();
+                
+                System.out.println("\n--------- DATA CAPTURED ---------");
+                System.out.println("- URL: " + doc.url() + " -");
+                System.out.println("- CHARACTERS: " + doc.charCount() + " -");
+                System.out.println("- IMAGES: " + doc.imgCount() + " -");
+                System.out.println("- SCRAPED BY: " + doc.scrapedBy() + " -");
+                System.out.println("- TIMESTAMP: " + doc.timestamp() + " -");
+                System.out.println("-----------------------------------");
+            } catch (Exception e){
+                System.out.println("Main testing thread interrupted.");
+                Thread.currentThread().interrupt();
+                break;
+            }
+        }
+        
+        // ponasanje iznad simulira upis u bazu
+        // TODO mongo writeri su preostali
     }
 }
--- src/main/java/workers/ScraperWorker.java
+++ src/main/java/workers/ScraperWorker.java
@@ -1,9 +1,63 @@
 package workers;
 
+import controller.CrawlerController;
+import data.ScrapedDocument;
+import java.time.Instant;
+import org.jsoup.nodes.Document;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 /**
  *
  * @author mihdjo
  */
-public class ScraperWorker {
+public class ScraperWorker implements Runnable{
     
+    private final String workerId;
+
+    private static final int POLITENESS_DELAY = 1500;
+    
+    public ScraperWorker(String workerId) {
+        this.workerId = workerId;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                String currentURL = CrawlerController.URL_QUEUE.take();
+                System.out.println(workerId + " is scraping: " + currentURL);
+                
+                try{
+                    Document doc = (Document) Jsoup.connect(currentURL).timeout(5000).get();
+                    
+                    int charCount = doc.text().length();
+                    int imgCount = doc.select("img").size();
+                    
+                    ScrapedDocument result = new ScrapedDocument(currentURL, charCount, imgCount, workerId, Instant.now());
+                    CrawlerController.RESULTS_QUEUE.put(result);
+                    
+                    Elements links = (Elements) doc.select("a[href]");
+                    
+                    for(Element link: links){
+                        String nextUrl = link.absUrl("href");
+                        
+                        if(nextUrl.startsWith("https://en.wikipedia.org/wiki") && !nextUrl.contains("#")) {
+                            if (CrawlerController.VISITED_SET.add(nextUrl)){
+                                CrawlerController.URL_QUEUE.put(nextUrl);
+                            }
+                        }
+                    }
+                } catch (Exception e){
+                    System.out.println(workerId + " failed to fetch " + currentURL + " - " + e.getMessage()); 
+                }
+                
+                Thread.sleep(POLITENESS_DELAY);
+            }
+        } catch (Exception e) {
+            Thread.currentThread().interrupt();
+            System.out.println(workerId + " was interrupted. Shutting down process.");
+        }
+    }
 }
