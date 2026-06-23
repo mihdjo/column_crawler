8ec403531ce99d9cf287c40f3fe1a2e5359f9ca5
mihdjo <mihailo.djokovic2003@gmail.com>
2026-06-23 14:59:47 +0200

    controller logic mongo redesign

--- src/main/java/controller/CrawlerController.java
+++ src/main/java/controller/CrawlerController.java
@@ -1,5 +1,12 @@
 package controller;
 
+import com.mongodb.MongoException;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.IndexOptions;
+import com.mongodb.client.model.Indexes;
 import data.ScrapedDocument;
 import java.util.Set;
 import java.util.concurrent.BlockingQueue;
@@ -7,6 +14,8 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
+import org.bson.Document;
+import workers.MongoWriterWorker;
 import workers.ScraperWorker;
 
 /**
@@ -26,38 +35,52 @@ public class CrawlerController {
         
         URL_QUEUE.add(seedURL);
         VISITED_SET.add(seedURL);
-        
+
         System.out.println("Initializing Column Crawler engine....");
-        
-        int numberOfScrapers = 3; 
-        ExecutorService scraperPool = Executors.newFixedThreadPool(numberOfScrapers);
-        
-        for(int i = 1; i <= numberOfScrapers; i++){
-            String workerId = "SCRAPER THREAD " + i;
-            scraperPool.execute(new ScraperWorker(workerId));
-        }
-        
-        System.out.println(numberOfScrapers + " scraper threads started successfully! \n");
-        
-        while(true){
-            try{
-                ScrapedDocument doc = RESULTS_QUEUE.take();
-                
-                System.out.println("\n--------- DATA CAPTURED ---------");
-                System.out.println("- URL: " + doc.url() + " -");
-                System.out.println("- CHARACTERS: " + doc.charCount() + " -");
-                System.out.println("- IMAGES: " + doc.imgCount() + " -");
-                System.out.println("- SCRAPED BY: " + doc.scrapedBy() + " -");
-                System.out.println("- TIMESTAMP: " + doc.timestamp() + " -");
-                System.out.println("-----------------------------------");
-            } catch (Exception e){
-                System.out.println("Main testing thread interrupted.");
-                Thread.currentThread().interrupt();
-                break;
+
+        String mongoURL = "mongodb://localhost:27017";
+
+        try (MongoClient mongoClient = MongoClients.create(mongoURL)) {
+            MongoDatabase database = mongoClient.getDatabase("column_crawler");
+            MongoCollection<Document> collection = database.getCollection("scraped_documents");
+
+            collection.createIndex(
+                    Indexes.ascending("url"),
+                    new IndexOptions().unique(true)
+            );
+            
+            int numberOfScrapers = 5;
+            int numberOfMongoWriters = 3;
+            
+            ExecutorService scraperPool = Executors.newFixedThreadPool(numberOfScrapers);
+            ExecutorService mongoWriterPool = Executors.newFixedThreadPool(numberOfMongoWriters);
+            
+            for (int i = 1; i <= numberOfScrapers; i++){
+                String workerId = "SCRAPER THREAD" + i;
+                scraperPool.execute(new ScraperWorker(workerId));
             }
+            
+            for(int i = 1; i < numberOfMongoWriters; i++){
+                String workerId = "MONGO WRITER" + i;
+                mongoWriterPool.execute(new MongoWriterWorker(workerId, collection));
+            }
+            
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                System.out.println("\n Shutdown signal received. Stopping crawler...");
+                scraperPool.shutdownNow();
+                mongoWriterPool.shutdownNow();
+            }));
+            
+            System.out.println(numberOfScrapers + " scraper threads started successfully!");
+            System.out.println(numberOfMongoWriters + " Mongo writer threads started successfully!");
+            System.out.println("Crawler running!!! CTRL + C to stop execution. \n");
+            
+            Thread.currentThread().join();
+        } catch (InterruptedException e){
+            Thread.currentThread().interrupt();
+            System.out.println("Main thread interrupted.");
+        } catch (MongoException e){
+            System.out.println("MongoDB error: " + e.getMessage());
         }
-        
-        // ponasanje iznad simulira upis u bazu
-        // TODO mongo writeri su preostali
     }
 }
